acac256f2f19befe10cd591c2ddc47dcd72a0651
alecsavvy <16739903+alecsavvy@users.noreply.github.com>
2025-04-29 16:36:02 -0600

    sdk example (#110)
    examples dr

---
+++ examples/README.md
@@ -0,0 +1,5 @@
+# examples
+
+The examples are set to run against the dev stack. To run the dev stack please see the instructions [here](../docs/developers.md).
+
+Once you have the dev stack running any of the examples should work by running `go run ./examples/${example}/main.go` from the root.
---
+++ examples/indexer/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"connectrpc.com/connect"
+	v1 "github.com/AudiusProject/audiusd/pkg/api/core/v1"
+	"github.com/AudiusProject/audiusd/pkg/sdk"
+)
+
+func main() {
+	sdk := sdk.NewAudiusdSDK("node1.audiusd.devnet")
+
+	height := int64(1)
+	for {
+		block, err := sdk.Core.GetBlock(context.Background(), connect.NewRequest(&v1.GetBlockRequest{
+			Height: height,
+		}))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("block: %+v", block)
+
+		height++
+	}
+}
