97ba95525e5b51d0b82615618756526f68e1067e
Danny <daniel@audius.co>
2025-09-19 13:30:46 -0700

    Fix links in proposal description (#198)

--- pkg/core/console/views/pages/adjudicate_page_templ.go
+++ pkg/core/console/views/pages/adjudicate_page_templ.go
@@ -92,7 +92,7 @@ Additional Proofs
 		props.Slash.Signature,
 		strippedEndpoint(props.ReportingEndpoint.Endpoint),
 		fmt.Sprintf(
-			"%s/adjudicate/%s%s",
+			"%s/console/adjudicate/%s?%s",
 			props.ReportingEndpoint.Endpoint,
 			props.ServiceProvider.Address,
 			getTimeRangeQueryString(props.StartTime, props.EndTime),
@@ -117,7 +117,7 @@ func formatAdditionalProofs(props *AdjudicatePageView) string {
 			signature,
 			strippedEndpoint(endpoint),
 			fmt.Sprintf(
-				"%s/adjudicate/%s%s",
+				"%s/console/adjudicate/%s?%s",
 				endpoint,
 				props.ServiceProvider.Address,
 				getTimeRangeQueryString(props.StartTime, props.EndTime),
