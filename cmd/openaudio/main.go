2b113b6809d71c5ac1db0ba7dbb8ed5227bfda99
Raymond Jacobson <ray@audius.co>
2025-10-07 16:26:49 -0700

    Add CORS header policy

--- cmd/openaudio/main.go
+++ cmd/openaudio/main.go
@@ -509,7 +509,7 @@ func setProtobufField(msgReflect protoreflect.Message, field protoreflect.FieldD
 func startEchoProxy(hostUrl *url.URL, logger *zap.Logger, coreService *coreServer.CoreService, storageService *server.StorageService, systemService *system.SystemService, ethService *eth.EthService) error {
 	e := echo.New()
 	e.HideBanner = true
-	e.Use(middleware.Logger(), middleware.Recover(), common.InjectRealIP())
+	e.Use(middleware.Logger(), middleware.Recover(), common.InjectRealIP(), common.CORS())
 
 	rpcGroup := e.Group("")
 	corePath, coreHandler := corev1connect.NewCoreServiceHandler(coreService, connectJSONOpt)
