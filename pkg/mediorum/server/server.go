f091e2cb07b0b3cf1cc2db0846882a400a2a5dcb
Raymond Jacobson <ray@audius.co>
2026-02-10 10:26:26 -0800

    Update eth fetching intervals

--- pkg/mediorum/server/server.go
+++ pkg/mediorum/server/server.go
@@ -665,7 +665,7 @@ func (ss *MediorumServer) startPprofServer(ctx context.Context) error {
 }
 
 func (ss *MediorumServer) refreshPeersAndSigners(ctx context.Context) error {
-	interval := 30 * time.Minute
+	interval := 10 * time.Minute
 	if os.Getenv("OPENAUDIO_ENV") == "dev" {
 		interval = 10 * time.Second
 	}
