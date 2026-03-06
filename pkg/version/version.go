e358e448aeb488aadce32ecc64e975ba84b47b52
Raymond Jacobson <ray@audius.co>
2026-03-06 15:59:59 -0800

    v1.2.3 Add versions ui to validators page on console

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"encoding/json"
 	"log"
+	"os"
 )
 
 //go:embed .version.json
@@ -14,6 +15,10 @@ var Version VersionJson
 func init() {
 	if err := json.Unmarshal(versionJSON, &Version); err != nil {
 		log.Fatalf("unable to parse .version.json file: %v", err)
+	}
+	// Allow override for local devnet testing (e.g. OPENAUDIO_VERSION_OVERRIDE=1.2.1)
+	if override := os.Getenv("OPENAUDIO_VERSION_OVERRIDE"); override != "" {
+		Version.Version = override
 	}
 }
 
