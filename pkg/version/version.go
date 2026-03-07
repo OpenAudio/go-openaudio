1504aed3343c63f986e4e46deea2c248de261b8a
Raymond Jacobson <ray@audius.co>
2026-03-06 16:05:50 -0800

    Remove override env var

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -4,7 +4,6 @@ import (
 	_ "embed"
 	"encoding/json"
 	"log"
-	"os"
 )
 
 //go:embed .version.json
@@ -15,10 +14,6 @@ var Version VersionJson
 func init() {
 	if err := json.Unmarshal(versionJSON, &Version); err != nil {
 		log.Fatalf("unable to parse .version.json file: %v", err)
-	}
-	// Allow override for local devnet testing (e.g. OPENAUDIO_VERSION_OVERRIDE=1.2.1)
-	if override := os.Getenv("OPENAUDIO_VERSION_OVERRIDE"); override != "" {
-		Version.Version = override
 	}
 }
 
